1b54f048daa40ba883858793aa702100e264ad26
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2021-01-03 16:49:26 -0600

    destructuring arrays

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -22,3 +26,35 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c); //2 3 4
+
+const [x, y, z] = arr;
+console.log(x, y, z); //2 3 4
+
+// const [first, second] = restaurant.categories;
+const [first, , second] = restaurant.categories;
+console.log(first, second);
+
+let [main, , secondary] = restaurant.categories;
+[main, secondary] = [secondary, main]; //switching variables
+console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
